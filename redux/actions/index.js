1367c9209c3de0268103b8556cfbfd8b9a74d6cb
Hien Le <43042242+hienle2703@users.noreply.github.com>
2020-08-19 22:40:14 +0700

    Update index.js

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -4,7 +4,7 @@ export const getApi = () => {
   return async (dispatch) => {
     try {
       const response = await fetch(
-        'https://gist.githubusercontent.com/nvthai/65dc75b2a575498d782748d4d0fabee4/raw/5742d9f7bb71abf4a1cc422c332b75a92e4ffad4/radio_songs.json',
+        'url stay here',
       );
       if (response.status === 200) {
         const data = await response.json();
